0c6c8905b361ac3d402e6522395a87949d942c55
Nursultan <nabakirov@GMAIL.COM>
2018-10-27 10:29:44 +0600

    tcp listener update

--- tcp_listener/tcp_listener/listener/request.py
+++ tcp_listener/tcp_listener/listener/request.py
@@ -7,8 +7,11 @@ from twisted.web.client import Headers
 
 def request(agent, url: str):
     logger.debug(url)
-    r = agent.request(b'GET', url.encode(), Headers({"Authorization": [TOKEN]}))
+    import requests
+    r = requests.get(url, headers={"Authorization": TOKEN})
+    logger.debug(r.text)
+    # r = agent.request(b'GET', url.encode(), Headers({"Authorization": [TOKEN]}))
     # r.addBoth(lambda p: logger.debug(p))
-    r.addErrback(lambda status: logger.error(status))
-    r.addCallback(lambda status: logger.debug("{}".format(status.code)))
+    # r.addErrback(lambda status: logger.error(status))
+    # r.addCallback(lambda status: logger.debug("{}".format(status.code)))
 
